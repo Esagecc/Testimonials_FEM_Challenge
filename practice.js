bbb86213aeaa22ff99f5c9c4b0fc5f76608720a4
Elizabeth Chavez <e.sage.cc@gmail.com>
2022-06-13 08:52:03 -0500

    updated the Readme file

---
+++ practice.js
@@ -0,0 +1,12 @@
+let movieArray = ['Heat', 'Insomnia', 'Savages']
+    for (let i = 0; i > movieArray.length; i++){
+    document.querySelector('h2').innerText += movie[i]
+}
+
+
+
+let numArray = [ 1, 2, 3, 4, 5, 6, 7]
+
+let i = 0; i > numArray.length; i++
+
+
